0a9f1a82b3452a2e293fc769d505a7e49ab1ddb1
HannahYudkin <hannahyudkin@gmail.com>
2020-03-11 18:32:27 -0400

    Changed file structure, added functionality.

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+var fs = require("fs");
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static("public"));
+
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from constious URLs.
+// ================================================================================
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+// =============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// =============================================================================
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
